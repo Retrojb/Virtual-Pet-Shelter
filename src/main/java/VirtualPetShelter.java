aeeb97d935abb42b859d641777722bffde443bdd
Retrojb <baltes.26@osu.edu>
2018-06-04 12:54:07 -0400

    Commit stream loop to get pet name

--- src/main/java/VirtualPetShelter.java
+++ src/main/java/VirtualPetShelter.java
@@ -13,6 +13,18 @@ public class VirtualPetShelter {
 		this.virtualPets = virtualPets;
 	}
 	
+	public VirtualPet fetchVirtualPet (String petName) {
+		return
+		virtualPets.stream()
+		.filter(pet -> pet.getPetName().equals(petName))
+		.findFirst()
+		.orElse(null);
+		//functional programming "loop"
+		// stream reads thru the byte streams, to gather the info out of the virtualPets, use filter
+		//to pass a lambda and apply it to all objects in the stream, and returns true. all other will not show.
+		// the '.' is a entire method in one line, spit across all the 3 lines and is it all the same method call.
+		//this loops through the whole list hold the loop, so this iterates over the list.
+	}
 	
 	
 }
